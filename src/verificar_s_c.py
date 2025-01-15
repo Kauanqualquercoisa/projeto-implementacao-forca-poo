707a05b2142ae221e02091718b67dc925ead1fbe
Kauanqualquercoisa <kauan.c.amor@gmail.com>
2025-01-15 16:05:05 -0300

    Adição do comando de voz e visualização do nome do usuário

--- src/verificar_s_c.py
+++ src/verificar_s_c.py
@@ -3,7 +3,14 @@ import sqlite3 as sql
 import database.start_db
 from menus import *
 
+import pyttsx3 # Importa a biblioteca para TTS (texto para fala)
 
+# Configuração do pyttsx3
+def falar(texto):
+    engine = pyttsx3.init()
+    engine.say(texto)
+    engine.runAndWait()
+    
 def verificar_s_c():
     
     cpf_jogador = str(input("\nDigite seu CPF: "))
@@ -12,13 +19,22 @@ def verificar_s_c():
     #conexão com o banco de dados
     banco = sql.connect(database.start_db.DIRETORIO_FINAL)
     cursor = banco.cursor()
-    cursor.execute("SELECT * FROM jogador WHERE senha = ? AND cpf = ?", (senha_jogador, cpf_jogador))
-
+    cursor.execute("SELECT nome FROM jogador WHERE senha = ? AND cpf = ?", (senha_jogador, cpf_jogador))
+    resultado = cursor.fetchone()
+    
     #   se encontrar o usuario correspondete ao cpf e senha inserido
-    if cursor.fetchone():
+    if resultado:  
+        # Se o jogador for encontrado
+        nome_jogador = resultado[0] #coloca o nome do jogador
+        
+        
+        mensagem = f"Bem-vindo, {nome_jogador}!"
+        print(mensagem)  
+        falar(mensagem)  #Fala a mensagem em áudio
+        
         # menu do jogo
-        mensagem = ''' 
-        --------- Menu Jogo da Forca – anonimo --------
+        mensagem = f'''
+        --------- Menu Jogo da Forca – {nome_jogador} --------
         1 – Jogar
         2 – Atualizar Dados
         3 – Voltar Menu Principal
@@ -32,6 +48,7 @@ def verificar_s_c():
         match opcao:
             case 1:
                 print('Jogar\n')
+                
             case 2:
                 print('Atualizar Dados\n')
             case 3:
