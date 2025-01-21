fde81243c903ddb64a84b198dd4e611dc74cbef7
Kauanqualquercoisa <kauan.c.amor@gmail.com>
2025-01-21 12:35:21 -0300

    Versão final do test de conversr de voz para texto

--- src/test.py
+++ src/test.py
@@ -1,20 +1,15 @@
 import speech_recognition as sr
 
-print("Microfones disponíveis:")
-print(sr.Microphone.list_microphone_names())
-
-
-microfone_index = sr.Microphone.list_microphone_names().index('Microfone (2- USB PnP Sound Device)')
 
 def reconhecer_audio():
     
      # inicializa o reconhecedor de voz criando uma instancia de sua classe
     reconhecedor = sr.Recognizer()
     
-    with sr.Microphone(device_index=microfone_index) as source:
+    with sr.Microphone() as source:
         
         print("Ajustando para ruído de fundo. Aguarde um momento...")
-        reconhecedor.adjust_for_ambient_noise(source, duration=7)
+        reconhecedor.adjust_for_ambient_noise(source, duration=3)
         
         print("Fale algo: ")
         
@@ -23,7 +18,7 @@ def reconhecer_audio():
             # reconhecedor.adjust_for_ambient_noise(source)
             
             # audio é um objeto que recebe uma informação de audio a partir do método listen()
-            audio = reconhecedor.listen(source, timeout=10, phrase_time_limit=10)
+            audio = reconhecedor.listen(source, timeout=4, phrase_time_limit=4)
             
             with open("teste_audio.wav", "wb") as f:
                 f.write(audio.get_wav_data())
@@ -32,10 +27,11 @@ def reconhecer_audio():
             texto = reconhecedor.recognize_google(audio, language='pt-BR')
             
         # Verifica se é uma única letra
-            if len(texto) == 1 and texto.isalpha():
-                print(f"Você falou a letra: {texto.upper()}")
-            else:
-                print("Por favor, fale apenas uma letra.")
+            print(texto)
+            #if len(texto) == 1 and texto.isalpha():
+              #  print(f"Você falou a letra: {texto.upper()}")
+            #else:
+              #  print("Por favor, fale apenas uma letra.")
         except sr.UnknownValueError:
             print("Não entendi o que você disse. Tente novamente.")
         except sr.RequestError as e:
